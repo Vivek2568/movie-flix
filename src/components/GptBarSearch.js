3a5bf8a4f604b2dede9acb9725dc884340100ec3
Vivek2568 <25prajapati68@gmail.com>
2024-06-24 12:11:06 +0530

    GPTsearch gemini API integrsstion

--- src/components/GptBarSearch.js
+++ src/components/GptBarSearch.js
@@ -1,11 +1,39 @@
+import { useRef } from "react";
+import { API_OPTIONS } from "../utils/constant";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI("AIzaSyCfjjCefNUK1yZnbMJ81ZZmcCq0_9Eazsk");
 const GptBarSearch = () => {
+    const searchText = useRef(null);
+    const fetchMoviefromTMDB = async (movie) => {
+        const data = await fetch('https://api.themoviedb.org/3/search/movie?query=' + movie + '&include_adult=false&language=en-US&page=1',
+            API_OPTIONS);
+        const json = await data.json();
+        return json.results;
+    };
+    const handleGptSearchClick = () => {
+        //console.log(searchText.current.value);
+        async function run() {
+            const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+            const prompt = "show only 5 name of result of" + searchText.current.value + "give only name in the form of array ";
+            const result = await model.generateContent(prompt);
+            const response = await result.response;
+            const text = response.text();
+            const movieNames=text.split(",");
+           // console.log(movieNames);
+            if (!text) return <p>sorry</p>;
+            const promiseArray = movieNames.map((movie) => fetchMoviefromTMDB(movie));
+            const tmdbresults= await Promise.all(promiseArray);
+            console.log(tmdbresults);
+        }
+        run();
+    };
     return (
         <div className=" bg-gray-300 ">
             <div className=" pt-[10%] flex justify-center">
-                <form className="w-1/2 grid grid-cols-12 ">
-                    <input type="text" className="  py-2 m-4 border-2 border-black col-span-9"
+                <form className="w-1/2 grid grid-cols-12 " onSubmit={(e) => e.preventDefault()} >
+                    <input ref={searchText} type="text" className="  py-2 m-4 border-2 border-black col-span-9"
                         placeholder="   What would you like to watch today?" />
-                    <button className="text-xl col-span-3  m-4  px-2 bg-red-500 border-2 border-black rounded-md">Search</button>
+                    <button className="text-xl col-span-3  m-4  px-2 bg-red-500 border-2 border-black rounded-md" onClick={handleGptSearchClick}>Search</button>
                 </form>
             </div>
         </div>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -7,3 +7,5 @@ export const API_OPTIONS = {
   };
 
   export const IMG_CDN_URL="https://image.tmdb.org/t/p/w300";
+
+  export const OPENAI_KEY="sk-proj-jpkeKuDZENfyD5zQkhfCT3BlbkFJGtq0XPcUhsAOFfQK4ErR";
